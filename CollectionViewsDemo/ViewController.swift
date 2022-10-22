fead0fc27d291cc9f6258ffe46d404ce59bad9af
Ayemere <davidodia4@gmail.com>
2022-10-22 13:21:10 +0100

    Initial Commit

---
+++ CollectionViewsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewsDemo
+//
+//  Created by Ayemere  Odia  on 2022/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
